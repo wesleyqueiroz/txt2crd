d31547b90ef25089a1add3ca6b2582c287d53092
Magnus Olsson <magnus.olsson@projectplace.com>
2013-01-03 17:08:39 +0100

    Added documentation

--- txt2crd.py
+++ txt2crd.py
@@ -13,6 +13,12 @@ def main():
         sys.exit(2)
 
 def convert2Chordpro(filePath):
+    """
+    Runs through the opened file and converts it line by line to chordpro format.
+
+    @param filePath: Path to opened input file.
+    @type filePath: string.
+    """
     inputFileStream = open(filePath, 'r')
     outputFileName = filePath.split("/")[-1][:-4] + ".crd"
     outputFileStream = open(outputFileName, 'w')
@@ -41,9 +47,35 @@ def convert2Chordpro(filePath):
     outputFileStream.close()
 
 def removeWhitespaces(line):
+    """
+    Replaces whitespaces in line with empty string.
+
+    @param line: Line to remove white spaces in.
+    @type line: string
+
+    @return: Line without whitespaces.
+    @returntype: string
+    """
     return line.replace(' ', '').replace('\n', '')
         
 def insertInLine(line, matches, positions):
+    """
+    Inserts chords into line according to specified positions.
+    E.g. if there's supposed to be a C in the line at position 10 and a 
+    Am at position 21 then this will inject "[C]" and "[Am]" into the correct place.
+
+    @param line: Text line to inject chords into.
+    @type line: string
+
+    @param matches: List of chords to inject.
+    @type matches: [string]
+
+    @param position: List of positions that matches the list of chords.
+    @type position: [int]
+
+    @return: Line with chords injected in it.
+    @returntype: string
+    """
     def insert(original, new, pos):
         return original[:pos] + new + original[pos:]
     i = 0
@@ -57,6 +89,15 @@ def insertInLine(line, matches, positions):
     return newLine
 
 def getChordMatches(line):
+    """
+    Checks line for occurrences of chords.
+
+    @param line: Text line to check for chords.
+    @type line: string
+
+    @return: A list of chords and a list of chord positions
+    @returntype: [string], [int]
+    """
     import re
 
     notes = "[ABCDEFG]";
