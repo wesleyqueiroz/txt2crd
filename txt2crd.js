8d2c63d6f746e17de86c9a8f055480da1fc4a027
Magnus Olsson <magnus.olsson@projectplace.com>
2013-02-01 17:41:34 +0100

    Added html and js template files for future online converter

---
+++ txt2crd.js
@@ -0,0 +1,6 @@
+function convertToChordpro() {
+    var lines = document.getElementById('textContent').value.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        alert(lines[i]);
+    }
+}
