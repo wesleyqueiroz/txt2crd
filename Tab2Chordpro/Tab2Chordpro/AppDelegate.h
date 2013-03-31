052504826372e7d848cc73cf532da1d8c447afd2
Magnus Olsson <magnus80a@gmail.com>
2013-03-31 13:36:33 +0200

    Added XCode project

---
+++ Tab2Chordpro/Tab2Chordpro/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Tab2Chordpro
+//
+//  Created by Magnus Olsson on 3/31/13.
+//  Copyright (c) 2013 Magnus Olsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
---
+++ Tab2Chordpro/Tab2ChordproTests/Tab2ChordproTests.h
@@ -0,0 +1,13 @@
+//
+//  Tab2ChordproTests.h
+//  Tab2ChordproTests
+//
+//  Created by Magnus Olsson on 3/31/13.
+//  Copyright (c) 2013 Magnus Olsson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tab2ChordproTests : SenTestCase
+
+@end
